cafe6978ef1757e8750f287b8be0ff28f9161b2c
Naasa Fikri <naasa.fikri@aol.com>
2019-01-26 22:55:32 +0700

    Edited configuration to match Wemos Lolin32 with built-in OLED display.

--- main/main.cpp
+++ main/main.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 using namespace std;
 
-OLED oled = OLED(GPIO_NUM_19, GPIO_NUM_22, SSD1306_128x64);
+OLED oled = OLED(GPIO_NUM_4, GPIO_NUM_5, SSD1306_128x64);
 
 void myTask(void *pvParameters) {
 	adc1_config_width(ADC_WIDTH_12Bit);
@@ -60,23 +60,23 @@ extern "C" {
 #endif
 void app_main() {
 
-	oled = OLED(GPIO_NUM_19, GPIO_NUM_22, SSD1306_128x64);
+	oled = OLED(GPIO_NUM_4, GPIO_NUM_5, SSD1306_128x64);
 	if (oled.init()) {
 		ESP_LOGI("OLED", "oled inited");
-//		oled.draw_rectangle(10, 30, 20, 20, WHITE);
-//		oled.select_font(0);
-//		//deprecated conversion from string constant to 'char*'
-//		oled.draw_string(0, 0, "glcd_5x7_font_info", WHITE, BLACK);
-//		ESP_LOGI("OLED", "String length:%d",
-//				oled.measure_string("glcd_5x7_font_info"));
-//		oled.select_font(1);
-//		oled.draw_string(0, 18, "tahoma_8pt_font_info", WHITE, BLACK);
-//		ESP_LOGI("OLED", "String length:%d",
-//				oled.measure_string("tahoma_8pt_font_info"));
-//		oled.draw_string(55, 30, "Hello ESP32!", WHITE, BLACK);
-//		oled.refresh(true);
-//		vTaskDelay(3000 / portTICK_PERIOD_MS);
-		xTaskCreatePinnedToCore(&myTask, "adctask", 2048, NULL, 5, NULL, 1);
+		oled.draw_rectangle(10, 30, 20, 20, WHITE);
+		oled.select_font(0);
+		//deprecated conversion from string constant to 'char*'
+		oled.draw_string(0, 0, "glcd_5x7_font_info", WHITE, BLACK);
+		ESP_LOGI("OLED", "String length:%d",
+				oled.measure_string("glcd_5x7_font_info"));
+		oled.select_font(1);
+		oled.draw_string(0, 18, "tahoma_8pt_font_info", WHITE, BLACK);
+		ESP_LOGI("OLED", "String length:%d",
+				oled.measure_string("tahoma_8pt_font_info"));
+		oled.draw_string(55, 30, "Hello ESP32!", WHITE, BLACK);
+		oled.refresh(true);
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
+		//xTaskCreatePinnedToCore(&myTask, "adctask", 2048, NULL, 5, NULL, 1);
 	} else {
 		ESP_LOGE("OLED", "oled init failed");
 	}
